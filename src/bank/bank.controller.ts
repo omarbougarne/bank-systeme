fb8aa614e7ee8a355c72b205e4a3dbf2f361659f
omarbougarne <omarbougarne98@gmail.com>
2024-12-19 23:50:29 +0100

    test

--- src/bank/bank.controller.ts
+++ src/bank/bank.controller.ts
@@ -1,5 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { BankService } from './bank.service';
+import { Bank } from './schema/bank.schema';
 
 @Controller('bank')
 export class BankController {
@@ -7,6 +8,29 @@ export class BankController {
     constructor(
         private bankService: BankService
     ) { }
+
+    // @Get('/')
+
+    @Post(':id/loan')
+    async giveLoan(@Param('id')
+    @Body() amount: number, id: string): Promise<Bank> {
+
+        return this.bankService.giveLoan(id, amount)
+    }
+
+    @Post(':id/collect')
+    async collectMoney(@Param('id')
+    @Body() amount: number, id: string): Promise<Bank> {
+
+        return this.bankService.collectMoney(id, amount)
+    }
+
+
+    @Patch('/:id')
+    async updateDetails(@Param('id')
+    @Body() id, amount) {
+        return this.bankService.updateDetails(id, amount)
+    }
 
 
 
